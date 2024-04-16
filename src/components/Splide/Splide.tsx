b67476eb42f899f8513f847079128fdcb4f6560e
Hillel1408 <etemax@bk.ru>
2024-04-16 23:53:01 +0300

    правки карусели логотипов

--- src/components/Splide/Splide.tsx
+++ src/components/Splide/Splide.tsx
@@ -5,7 +5,7 @@ import "keen-slider/keen-slider.min.css";
 import classNames from "classnames";
 
 export default function Splide({ className }: { className?: string }) {
-  const animation = { duration: 100000, easing: (t: any) => t };
+  const animation = { duration: 10000, easing: (t: any) => t };
 
   const logo = [
     "img/logo-3.png",
@@ -18,12 +18,25 @@ export default function Splide({ className }: { className?: string }) {
     "img/logo-10.png",
     "img/logo-11.png",
     "img/logo-12.png",
+    "img/logo-3.png",
+    "img/logo-4.png",
+    "img/logo-5.png",
+    "img/logo-6.png",
+    "img/logo-7.png",
+    "img/logo-8.png",
+    "img/logo-9.png",
+    "img/logo-10.png",
+    "img/logo-11.png",
+    "img/logo-12.png",
   ];
 
-  const [sliderRef] = useKeenSlider({
+  const [sliderRef] = useKeenSlider<HTMLDivElement>({
     loop: true,
-    renderMode: "performance",
     drag: false,
+    mode: "snap",
+    rtl: false,
+    slides: { perView: "auto" },
+    renderMode: "performance",
     created(s) {
       s.moveToIdx(5, true, animation);
     },
@@ -43,14 +56,12 @@ export default function Splide({ className }: { className?: string }) {
         className
       )}
     >
-      {new Array(2).fill("").map((item, index) => (
+      {logo.map((item, index) => (
         <div
           key={index}
-          className="animation-3 keen-slider__slide flex [&>img]:h-[7.2rem] md:[&>img]:h-[6.4rem]"
+          className="animation-3 keen-slider__slide justify-center flex min-w-[200px]"
         >
-          {logo.map((item, index) => (
-            <img key={index} className="" src={item} alt="" />
-          ))}
+          <img key={index} className="" src={item} alt="" />
         </div>
       ))}
     </div>

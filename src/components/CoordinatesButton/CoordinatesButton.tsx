8845248c784ba9c2b4dbe052e4faa565814a5f08
Hillel1408 <etemax@bk.ru>
2024-04-18 15:29:56 +0300

    правки

--- src/components/CoordinatesButton/CoordinatesButton.tsx
+++ src/components/CoordinatesButton/CoordinatesButton.tsx
@@ -3,12 +3,15 @@
 import { useState, useEffect } from "react";
 import classNames from "classnames";
 import { Button } from "@/components";
+import { useWindowSize } from "@/hooks/windowSize";
 
 export default function ({ myRef, href }: { myRef: any; href: string }) {
   const [coordinates, setCoordinates] = useState({
     clientX: 0,
     clientY: 0,
   });
+
+  const size = useWindowSize();
 
   useEffect(() => {
     function setCursor(e: any) {
@@ -31,21 +34,27 @@ export default function ({ myRef, href }: { myRef: any; href: string }) {
         "absolute top-0 bottom-0 left-0 right-0 z-20 overflow-hidden"
       )}
     >
-      <div
-        style={{
-          top: `${coordinates.clientY}px`,
-          left: `${coordinates.clientX}px`,
-        }}
-        className={classNames(
-          "absolute",
-          coordinates.clientY > myRef?.current?.clientHeight ||
-            coordinates.clientY <= 0
-            ? "hidden"
-            : ""
-        )}
-      >
-        <Button text="Подробнее" />
-      </div>
+      {size.width && size.width < 767 ? (
+        <div className="w-[4rem] h-[3.2rem] bg-[#d5ff0c] absolute right-[2rem] bottom-[2rem] flex items-center justify-center text-2xl">
+          →
+        </div>
+      ) : (
+        <div
+          style={{
+            top: `${coordinates.clientY}px`,
+            left: `${coordinates.clientX}px`,
+          }}
+          className={classNames(
+            "absolute",
+            coordinates.clientY > myRef?.current?.clientHeight ||
+              coordinates.clientY <= 0
+              ? "hidden"
+              : ""
+          )}
+        >
+          <Button text="Подробнее" />
+        </div>
+      )}
       <a
         href={href}
         target="_blank"
